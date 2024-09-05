391fbbc5677bd0bfe4c38f3e7782bf2e0c3f181f
MerlinSchwethelm <merlin.schwethelm@johnnybytes.com>
2024-09-05 12:04:33 +0200

    run code analysis

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -12,6 +12,7 @@ class VerifyEmailController extends Controller
 {
     /**
      * Mark the authenticated user's email address as verified.
+     *
      * @throws Exception
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
