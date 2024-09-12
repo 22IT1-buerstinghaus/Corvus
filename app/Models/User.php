75a3440313f36e1eced6a8d5a7f2e200427f90f0
MerlinSchwethelm <merlin.schwethelm@johnnybytes.com>
2024-09-12 10:13:13 +0200

    feat: add User Resurce Panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,6 +55,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'email_verified_at',
+        'is_locked',
+        'is_admin',
     ];
 
     /**
