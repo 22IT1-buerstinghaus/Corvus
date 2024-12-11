df677508252ad4a51337e5f0bc218a19d26cc138
MerlinSchwethelm <merlin.schwethelm@johnnybytes.com>
2024-12-11 18:17:21 +0100

    change redirect

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/admin');
+    return redirect('/admin/login');
 });
 //
 Route::get('/dashboard', function () {
